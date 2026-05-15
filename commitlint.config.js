17b2bb58eaed47cbf914a9546f442df87862da89
Luca Bagante <luca.bagante@gmail.com>
2026-05-15 09:52:49 +0200

    chore: bootstrap release tooling and conventions

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 120],
+    'body-max-line-length': [1, 'always', 200], // warning, not error
+  },
+};
